5dd53338f458249381e3638965e8a5bf32354c5d
guilhermeB-gomes <guilherme.gomes154@etec.sp.gov.br>
2024-08-08 16:01:28 -0300

    save04

--- teste/produtos.js
+++ teste/produtos.js
@@ -72,58 +72,59 @@ const product = [
         price: 100,
     },
 ];
-const categories =[...new Set(product.map((item)=>
-    {return item}))]
-    let i=0;
-document.getElementById('root').innerHTML = categories.map((item)=>
-{
-    var{image, title, price} = item;
-    return(
-        `<div class='box'
-          <div class='img-box'>
-                <img class='images' src=${image}></img>
-                </div>
+
+const categories = [...new Set(product.map((item) => item))];
+let i = 0;
+
+document.getElementById('root').innerHTML = categories.map((item) => {
+    var { image, title, price } = item;
+    return (
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image} />
+            </div>
             <div class='bottom'>
-            <p>${title}</p>
-            <h2>$ ${price}.00</h2>`+
-            "<button onclick='produtos("+(i++)+")'>Add to cart</button>"+
-            `</div>
-            </div>`
-    )
-}).join('')
+                <p>${title}</p>
+                <h2>$ ${price}.00</h2>
+                <button onclick='produtos(${i++})'>Add to cart</button>
+            </div>
+         </div>`
+    );
+}).join('');
 
-var cart =[];
+var cart = [];
 
-function produtos(a){
-    cart.push({...categories[a]});
+function produtos(a) {
+    cart.push({ ...categories[a] });
     displaycart();
 }
-function delElement(a){
+
+function delElement(a) {
     cart.splice(a, 1);
     displaycart();
 }
 
-function displaycart(a){
-    let j = 0, total=0;
-    document.getElementById("count").innerHTML=cart.length;
-    if(cart.length==0){
+function displaycart() {
+    let j = 0, total = 0;
+    document.getElementById("count").innerHTML = cart.length;
+    
+    if (cart.length == 0) {
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
-        document.getElementById("total").innerHTML = "$"+0+".00";
-    }
-    else{
-        document.getElementById("cartItem").innerHTML = cart.map((items)=>
-        {
-            var {image, title, price} = items;
-            total=total+price;
-            document.getElementById("total").innerHTML = "$ "+total+".00";
-            return(
+        document.getElementById("total").innerHTML = "$0.00";
+    } else {
+        document.getElementById("cartItem").innerHTML = cart.map((items) => {
+            var { image, title, price } = items;
+            total += price;
+            document.getElementById("total").innerHTML = "$ " + total + ".00";
+            return (
                 `<div class='cart-item'>
                     <div class='row-img'>
-                        <img class='rowimg' src=${image}>
+                        <img class='rowimg' src=${image} />
                     </div>
                     <p style='font-size:12px;'>${title}</p>
-                    <h2> style='font-size: 15px;'>$ ${price}.00</h2>`+
-                    "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+                    <h2 style='font-size: 15px;'>$ ${price}.00</h2>
+                    <i class='fa-solid fa-trash' onclick='delElement(${j++})'></i>
+                </div>`
             );
         }).join('');
     }
